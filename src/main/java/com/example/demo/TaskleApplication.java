847b94756f79e43477b9bf4456fd20b4552db32d
髙山寛 <kanstephentakayama@gmail.com>
2024-10-06 22:54:37 +0900

    first commit

---
+++ src/main/java/com/example/demo/TaskleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskleApplication.class, args);
+	}
+
+}
